f58b147b1927115ca7389785170a43f9bd46dd40
bentondavidl <bentondavidl@gmail.com>
2021-12-02 20:49:56 -0500

    figure out legend separation

--- data testing.R
+++ data testing.R
@@ -3,8 +3,6 @@
 library(tidyverse)
 library(jsonlite)
 library(ggplot2)
-library(egg)
-library(plotly)
 
 # Employment data from department of labor oui.doleta.gov/unemployment/claims.asp
 unemployment <- fromJSON("./data/us_unemployment.txt")$r539cyState$week %>%
@@ -47,25 +45,49 @@ covid <- merged %>%
 
 joined <- inner_join(covid, unemployment, by=c("Province_State"="stateName","date"="weekEnded"))
 filtered <- policy_data %>%
-  filter(province=="North Carolina")
+  filter(province=="North Carolina") %>%
+  mutate(y=1)
 
-# Unemployment chart
-p1 <- joined %>%
-  filter(type=="cases",Province_State=="North Carolina") %>%
-  pivot_longer(c("InitialClaims","ContinuedClaims"), names_to="claim_type", values_to="claims") %>%
-  ggplot(aes(x=date, y=claims, fill=claim_type)) + 
-  geom_bar(stat="identity") + 
-  geom_vline(data=filtered,mapping=aes(xintercept=date_start))
+# # Unemployment chart
+# p1 <- joined %>%
+#   filter(type=="cases",Province_State=="North Carolina") %>%
+#   pivot_longer(c("InitialClaims","ContinuedClaims"), names_to="claim_type", values_to="claims") %>%
+#   ggplot(aes(x=date, y=claims, fill=claim_type)) + 
+#   geom_bar(stat="identity") + 
+#   geom_vline(data=filtered,mapping=aes(xintercept=date_start))
+# 
+# # Cases chart
+# p2 <- joined %>%
+#   filter(Province_State=="North Carolina") %>%
+#   ggplot(aes(x=date,y=new_value,fill=type)) + 
+#   geom_area() +
+#   geom_vline(data=filtered,mapping=aes(xintercept=date_start))
 
-# Cases chart
-p2 <- joined %>%
-  filter(Province_State=="North Carolina") %>%
-  ggplot(aes(x=date,y=new_value,fill=type)) + 
-  geom_area() +
-  geom_vline(data=filtered,mapping=aes(xintercept=date_start))
+legends <- c(l1=NULL)
+g_legend<-function(a.plot){
+  tmp <- ggplotGrob(a.plot)
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  legend
+}
 
-# stacked charts
-ggp <- ggarrange(p1, p2, ncol=1, heights=c(3,1))
+g1 <- filtered %>%
+  ggplot(aes(x=date_start,y=y,size=5,alpha=.5,color=dist_index_med_est)) + 
+  geom_point() +
+  # coord_cartesian(xlim=date_lim$x, expand=F) + 
+  guides(alpha=FALSE, size=FALSE, color=guide_colorbar(title="Measure Strictness")) +
+  theme(
+    axis.ticks = element_blank(), 
+    axis.title = element_blank(),
+    axis.text = element_blank(),
+    plot.margin = unit(c(0,0.25,0,2.75),"cm"),
+    panel.background = element_blank()
+  )
+legends$l1 <- g_legend(g1)
+g1+theme(legend.position = "none")
 
+plot(legends$l1, xlim=c(0,legends$l1$widths[3]), ylim=c(0,legends$l1$heights[3]))
+
+legends$l1
 
   
